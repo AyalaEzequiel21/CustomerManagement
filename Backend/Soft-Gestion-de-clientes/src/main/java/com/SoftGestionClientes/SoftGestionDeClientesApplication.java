85e88d1d38ea0db400f60ee0e2a3a7f8b1691d12
Checho <ezequielayala.c@gmail.com>
2023-08-06 21:50:14 -0300

    first commit, creation proyect

---
+++ Backend/Soft-Gestion-de-clientes/src/main/java/com/SoftGestionClientes/SoftGestionDeClientesApplication.java
@@ -0,0 +1,13 @@
+package com.SoftGestionClientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftGestionDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftGestionDeClientesApplication.class, args);
+	}
+
+}
