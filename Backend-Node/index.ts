c80a9486fee04806105cc8f6e557bc3923118859
Checho <ezequielayala.c@gmail.com>
2023-11-11 18:23:33 -0300

    updates

--- Backend-Node/index.ts
+++ Backend-Node/index.ts
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(cookieParser())
 app.use(express.json())
-app.use(cors({origin: "http://localhost:3000", credentials: true}))
+app.use(cors({origin: process.env.API_URL, credentials: true}))
 app.use('/praderaAPI', routes)
 app.use(errorHandler)
 
