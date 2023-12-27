895608148c1558dbbd006419430619148e57d275
Checho <ezequielayala.c@gmail.com>
2023-12-27 20:35:56 -0300

    change cors

--- Backend-Node/index.ts
+++ Backend-Node/index.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import routes from './routes'
 import {connectDB} from "./db/connect"
-// import cors from 'cors'
+import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import { errorHandler } from "./middlewares/error.middleware"
 
@@ -9,16 +9,11 @@ import { errorHandler } from "./middlewares/error.middleware"
 
 const app = express()
 
+const urlList = ['http://localhost:5173']
 
 app.use(cookieParser())
 app.use(express.json())
-// app.use(cors({origin: "*", credentials: true}))
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*'])
-    res.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    res.append('Access-Control-Allow-Headers', 'Content-Type')
-    next()
-})
+app.use(cors({origin: urlList, credentials: true}))
 app.use('/praderaAPI', routes)
 app.use(errorHandler)
 
